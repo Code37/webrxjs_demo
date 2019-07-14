b0e017900d9bef54eb55fb7d89879439f1fc2eab
Code37 <495673131@qq.com>
2019-07-14 13:09:27 -0400

    Basic Service

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+// import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 // const DISHES: Dish[] = [
 //   {
@@ -54,14 +55,15 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[];
 
   // selectedDish: Dish = DISHES[0]; // first dish is chosen
   selectedDish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
 
   onSelect(dish: Dish) {
