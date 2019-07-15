154d643b88ef843538e912c1765723d0aaf2e7ed
Code37 <495673131@qq.com>
2019-07-15 14:24:41 -0400

    Promise Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -65,7 +65,8 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     const id = this.route.snapshot.params.id;
-    this.dish = this.dishservice.getDish(id);
+    this.dishservice.getDish(id).then(dish => this.dish = dish);
+    // this.dish = this.dishservice.getDish(id);
   }
 
   goBack(): void {
--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -8,16 +8,48 @@ import { PROMOTIONS } from '../shared/promotions';
 export class PromotionService {
 
   constructor() { }
+  // getDishes(): Promise<Dish[]> {
+  //   return new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //     setTimeout(() => resolve(DISHES), 2000);
+  //   });
+  // }
+  //
+  // getDish(id: string): Promise<Dish> {
+  //   return new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //     setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+  //   });
+  // }
+  //
+  // getFeaturedDish(): Promise<Dish> {
+  //   return  new Promise(resolve=> {
+  //     // Simulate server latency with 2 second delay
+  //     setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+  //   });
+  // }
 
-  getPromotions(): Promotion[] {
-    return PROMOTIONS;
+  getPromotions(): Promise<Promotion[]> {
+    return Promise.resolve(PROMOTIONS);
   }
 
-  getPromotion(id: string): Promotion {
-    return PROMOTIONS.filter((promo) => (promo.id === id))[0];
+  getPromotion(id: string): Promise<Promotion> {
+    return Promise.resolve(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]);
   }
 
-  getFeaturedPromotion(): Promotion {
-    return PROMOTIONS.filter((promotion) => promotion.featured)[0];
+  getFeaturedPromotion(): Promise<Promotion> {
+    return Promise.resolve(PROMOTIONS.filter((promotion) => promotion.featured)[0]);
   }
+
+  // getPromotions(): Promotion[] {
+  //   return PROMOTIONS;
+  // }
+  //
+  // getPromotion(id: string): Promotion {
+  //   return PROMOTIONS.filter((promotion) => (promotion.id === id))[0];
+  // }
+  //
+  // getFeaturedPromotion(): Promotion {
+  //   return PROMOTIONS.filter((promotion) => promotion.featured)[0];
+  // }
 }
